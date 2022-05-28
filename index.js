2b6fe6ce103fd871d08c24ed8a45a76140dd7c1e
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-28 15:34:35 +0600

    user get

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.agngo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
     try {
       await client.connect();
@@ -54,18 +69,24 @@ async function run() {
         res.send(result);
     });
 
-    app.get('/orders'/* , verifyJWT */, async (req, res) => {
+    app.get('/orders',verifyJWT, async (req, res) => {
       const email = req.query.email;
-     // const decodedEmail = req.decoded.email;
-    //  if (patient === decodedEmail) {
+      const decodedEmail = req.decoded.email;
+      if (email === decodedEmail) {
         const query = { email: email };
         const orders = await orderCollection.find(query).toArray();
-       /*  return */ res.send(orders);
-    //  }
-    /*   else {
+         return  res.send(orders);
+     }
+       else {
         return res.status(403).send({ message: 'forbidden access' });
-      } */
+      } 
     });
+    app.get('/user',  verifyJWT,  async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    
   
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
